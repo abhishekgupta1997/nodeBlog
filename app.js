9274ba6202484df9b2520fa5d48b4ebc3e5b14a0
Abhishek Gupta <dipakgupta199518@gmail.com>
2022-05-28 21:59:17 +0530

    changes for hosting

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const { create, result } = require('lodash');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const { render } = require('express/lib/response');
-const blogRoutes = require('./routes/blogRoutes')
+const blogRoutes = require('./routes/blogRoutes');
+const path = require('path');
 
 //express app
 const app = express();
 
 //setting up dynamic port
-dotenv.config({path:'config.env'})
+dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 3000
 
 //connect to db
@@ -20,7 +21,7 @@ const dbURI = 'mongodb+srv://blog:blog321@cluster0.6zhde.mongodb.net/nodeNinja?r
 mongoose.connect(dbURI)
     .then((result) => {
         console.log('connected to db');
-        app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+        app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) });
         // listen to req once db connection is established
     })
     .catch((err) => console.log(err));
@@ -29,21 +30,22 @@ mongoose.connect(dbURI)
 app.set('view engine', 'ejs');
 
 //middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.resolve(__dirname, "public")));
+// app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'));
 
 //routes
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
 
-app.get('/about',(req,res)=>{
-    res.render('about',{title:'About'});
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
 });
 
 //blog routes
-app.use('/blogs',blogRoutes);
+app.use('/blogs', blogRoutes);
 
 //redirect
 // router.get('/about-us',(req,res)=>{
@@ -51,7 +53,7 @@ app.use('/blogs',blogRoutes);
 // })
 
 //404
-app.use((req,res)=>{
+app.use((req, res) => {
     // res.status(404).sendFile('./views/404.html', {root:__dirname});
-    res.status(404).render('404',{title:'404'});
+    res.status(404).render('404', { title: '404' });
 })
