4a5a661f9fbd8552a8f81d8634d1db56e1442e67
Abhishek Gupta <dipakgupta199518@gmail.com>
2022-05-28 19:38:33 +0530

    added MVC architecture

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const { create, result } = require('lodash');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
 const { render } = require('express/lib/response');
+const blogRoutes = require('./routes/blogRoutes')
 
 //express app
 const app = express();
@@ -27,111 +27,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'));
 
-//mongoose and mongo sandbox routes
-// app.get('/add-blog',(req,res) =>{
-//     const blog = new Blog ({
-//         title:'new blog',
-//         snippet:'about my new blog',
-//         body:'more about my new blog'
-//     });
-
-//     blog.save()
-//         .then((result) =>{
-//             res.send(result)
-//         })
-//         .catch((err) =>{
-//             console.log(err);
-//         });
-// })
-
-// app.get('/all-blogs',(req,res)=>{
-//     Blog.find()
-//         .then((result)=>{
-//             res.send(result);
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         })
-// })
-
-// app.get('single-blog',(req,res)=>{
-//     Blog.findById('628b7fed5b71d8261a21c14b')
-//         .then((result)=>{
-//             res.send(result)
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         });
-// })
-
 //routes
 app.get('/',(req,res)=>{
-    // res.send('<h1>Homepage</h1>');
-    // res.sendFile('./views/index.html', {root:__dirname});
-    // const blogs = [
-    //     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    //     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    //     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    //   ];
-    // res.render('index', {title:'Home',blogs});
     res.redirect('/blogs');
 });
 
 app.get('/about',(req,res)=>{
-    // res.send('<h1>about page</h1>');
     res.render('about',{title:'About'});
 });
 
 //blog routes
-app.get('/blogs',(req,res)=>{
-    Blog.find().sort({createdAt:-1})
-        .then((result)=>{
-            res.render('index',{title:'All Blogs', blogs:result})
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-})
-
-app.post('/blogs',(req,res)=>{
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result)=>{
-            res.redirect('/blogs');
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-})
+app.use('/blogs',blogRoutes);
 
 //redirect
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
-})
-
-app.get('/blogs/create',(req,res)=>{
-    res.render('create',{title:'Create a new Blog'});
-})
-
-app.get('/blogs/:id',(req,res)=>{
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result =>{
-            res.render('details', {blog:result, title:'Blog Details'});
-        })
-        .catch(err => console.log(err));
-});
-
-app.delete('/blogs/:id',(req,res)=>{
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result=>{
-            res.json({redirect:'/blogs'})
-        })
-        .catch(err=> console.log(err));
-});
-
-
+// router.get('/about-us',(req,res)=>{
+//     res.redirect('/about');
+// })
 
 //404
 app.use((req,res)=>{
